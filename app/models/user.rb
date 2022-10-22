3811b54965fa5af9eef51b01cfb647261a34c3a3
Artem Skrynnyk <skrynnyk.artem@coaxsoft.com>
2022-10-22 20:44:02 +0300

    Added html validations for the Devise's views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  attr_accessor :_rucaptcha
+
   after_create_commit :create_cart!
 
   devise :database_authenticatable, :registerable, :confirmable,
