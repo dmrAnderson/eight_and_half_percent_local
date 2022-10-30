f8e6f3d82c500f45e08af881905c63a0b393ed5a
Artem Skrynnyk <skrynnyk.artem@coaxsoft.com>
2022-10-30 14:29:20 +0200

    5 Breaking up route

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  devise_for :users,
-             path_names: {
-               sign_in: 'sign-in',
-               sign_up: 'sign-up'
-             },
-             controllers: {
-               confirmations: 'users/confirmations',
-               omniauth_callbacks: 'users/omniauth_callbacks',
-               passwords: 'users/passwords',
-               registrations: 'users/registrations',
-               sessions: 'users/sessions',
-               unlocks: 'users/unlocks'
-             }
+  draw(:users)
 
   resources :products
 
